56d3e85f0a259908f07ce40fe3cf890f86cfd3a0
Job Alvarez <jobitoalv@gmail.com>
2023-01-12 11:15:00 +1000

    navbar div class

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,10 @@ import logo from './logo.svg'
 const Navbar = () => {
   return <nav>
     <div className='nav-center'>
-      <div className='nav-header'></div>
+      <div className='nav-header'>
+        <img src={logo} alt="logo"/>
+        <button className='nav-to'></button>
+      </div>
       <div className='links-container show-container'></div>
       <ul className='social-icon'></ul>
     </div>
